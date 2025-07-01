b4abfdd3e6def24edcc77cfe5ea87a5d7503cb6c
piktorwa <wiktorpantak11@gmail.com>
2025-07-01 21:13:28 +0200

    Added fpga project folder

---
+++ masters_project/masters_project.srcs/sources_1/new/kv260_rtl.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.07.2025 20:15:02
+// Design Name: 
+// Module Name: kv260_rtl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module kv260_rtl(
+
+    );
+    
+    
+endmodule
